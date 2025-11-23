d1d5d3e2bf35ef117b263b33d4f9f23192073b2e
Emma Andersson <emma.andersson.se@outlook.com>
2025-11-23 13:54:46 +0100

    Added words in wordList.js

--- src/storage-file/wordList.js
+++ src/storage-file/wordList.js
@@ -1,3 +1,7 @@
-const wordList = ["example1", "example2", "examplethree"]
+export const word = [
+    "APPLE", "BANANA", "ORANGE", "PEAR", "GRAPE", "CHERRY", "STRAWBERRY", "BLUEBERRY", "RASPBERRY", "BLACKBERRY", "CRANBERRY", "WATERMELON", "CANTALOUPE", "PINEAPPLE", "MANGO", "PAPAYA", "KIWI", "LEMON", "LIME", "GRAPEFRUIT", "POMEGRANATE", "PEACH", "NECTARINE", "APRICOT", "PLUM", "FIG", "DATE", "COCONUT", "AVOCADO", "PASSIONFRUIT", "DRAGONFRUIT", "GUAVA", "STARFRUIT", "JACKFRUIT", "MULBERRY", "ELDERBERRY", "PERSIMMON", "MANDARIN"
+] 
+    // Array of fruits. Fruit for astronauts to get vitamins :)
 
-export {wordList}
+console.log("Total fruits: ${word.length}, First fruit: ${word[0]}");
+    // Log the total number of fruits and the first fruit
